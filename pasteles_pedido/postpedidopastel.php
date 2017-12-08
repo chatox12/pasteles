25f6fb35e631e8b64c7e565a45acec5f6d4eae78
chatox <chatoxnew12@hotmail.com>
2017-12-08 10:39:20 -0600

    eliminando codigo basura

--- pasteles_pedido/postpedidopastel.php
+++ pasteles_pedido/postpedidopastel.php
@@ -2,6 +2,6 @@
 include_once('../api.php');
 
     $api = new api();
-    echo $api->postCliente($_GET["cantidad"],$_GET["rebanadas"],$_GET["idpasteles"],$_GET["idpedido"]);
+    echo $api->postPedidoPastel($_GET["cantidad"],$_GET["rebanadas"],$_GET["idpasteles"],$_GET["idpedido"]);
 
 ?>

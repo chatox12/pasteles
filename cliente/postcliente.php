b586cbfcdccda5f56006b608e602bca78917f6e1
chatox <chatoxnew12@hotmail.com>
2017-12-14 09:05:17 -0600

    agregando postcliente

--- cliente/postcliente.php
+++ cliente/postcliente.php
@@ -2,6 +2,6 @@
 include_once('../api.php');
 
     $api = new api();
-    echo $api->postCliente($_GET["nombre"],$_GET["apellido"],$_GET["direccion"],$_GET["telefono"]);
+    echo $api->postCliente($_GET["nombre"],$_GET["apellido"],$_GET["direccion"],$_GET["telefono"],$_GET["nickname"],$_GET["password"],$_GET["correo"],$_GET["nit"]);
 
 ?>
